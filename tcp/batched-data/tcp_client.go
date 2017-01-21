dc1a61194fb0a571177cce92b0999d9a0b15650c
Vineet John <vinjohn10@gmail.com>
2017-01-21 16:39:22 -0500

    Added test for batched TCP transmissions

--- tcp/batched-data/tcp_client.go
+++ tcp/batched-data/tcp_client.go
@@ -11,15 +11,20 @@ func runExperiment() int64 {
 	var i = 0
 
 	start := time.Now()
-	a := make([]byte, 100)
-	for i < 10 {
+	a := make([]byte, 1)
+	for i < 100 {
 
 		conn, err0 := net.Dial("tcp", "127.0.0.1:8082")
 		if err0 != nil {
 			fmt.Println(err0)
 			os.Exit(1)
 		}
-		conn.Write(a)
+
+		var j = 0
+		for j < 10 {
+			conn.Write(a)
+			j += 1
+		}
 
 		conn.Close()
 		i += 1
