89a674d4e8c4145e401bb32c7698cdb3623dc201
Vineet John <vinjohn10@gmail.com>
2017-01-22 23:27:24 -0500

    Fixed TCP bug for acks

--- tcp/single-connection/client.go
+++ tcp/single-connection/client.go
@@ -8,29 +8,48 @@ import "fmt"
 
 func runExperiment() int64 {
 
-	a := make([]byte, 1000000)
+	messageSize := 10000
+	msg := make([]byte, messageSize)
+	i := 0
+	for i < len(msg) {
+		msg[i] = 'a'
+		i += 1
+	}
+
+	ack := make([]byte, 100)
 
 	start := time.Now()
 
 	conn, err0 := net.Dial("tcp", "127.0.0.1:8082")
+	defer conn.Close()
+
 	if err0 != nil {
 		fmt.Println(err0)
 		os.Exit(1)
 	}
-	conn.Write(a)
-	conn.Close()
+	conn.Write(msg)
 
-	log.Printf("Took %d Microseconds", (time.Since(start).Nanoseconds())/1000)
-	return time.Since(start).Nanoseconds()
+	n, _ := conn.Read(ack)
+	s := string(ack[:n])
+	if s == "Message received" {
+		elapsed := time.Since(start).Nanoseconds()
+		log.Printf("Took %d Microseconds", elapsed/1000)
+		return elapsed / 1000
+	} else {
+		log.Println("Failed to receive ack")
+		return 0
+	}
 }
 
 func main() {
 	var timeTrack int64
+	var numExperiments = 10
+
 	i := 0
-	for i < 10 {
+	for i < numExperiments {
 		timeTrack += runExperiment()
 		i = i + 1
 	}
 
-	log.Printf("Took %d Microseconds, on average", (timeTrack/10)/1000)
+	log.Printf("Took %d Microseconds, on average", timeTrack/int64(numExperiments))
 }
--- tcp/single-connection/server.go
+++ tcp/single-connection/server.go
@@ -11,20 +11,38 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer ln.Close()
 
-	tmp := make([]byte, 1000000)
 	for {
-
 		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		_, err = conn.Read(tmp)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	numBytesExpected := 10000
+
+	buf := make([]byte, numBytesExpected)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	var reply []byte
+	if n == numBytesExpected {
+		reply = []byte("Message received")
+	} else {
+		reply = []byte("Dropped packets")
+	}
+
+	conn.Write(reply)
+	conn.Close()
+
+	fmt.Println("Read ", n, " bytes")
 }
