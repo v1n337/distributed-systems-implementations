3bffa5bdb5863e415ac4931fb90aed7cb686d303
Vineet John <vinjohn10@gmail.com>
2017-02-06 21:25:03 -0500

    Simple Hello World program

--- nfs/src/main.cpp
+++ nfs/src/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
+#include <fuse.h>
 
 int main(int argc, char* argv[])
 {
-	std::string sshfscmd ("sshfs ");
-	std::string command = sshfscmd + argv[1] + " " + argv[2];
-	std::cout << command << std::endl;
-	const char * c = command.c_str();
-	system(c);
+	std::cout << "Hello World" << std::endl;
 }
